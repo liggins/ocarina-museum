2e61551cc759e36039edba8d6e3b77fa9d287503
Niall Caplis <niallcaplis@gmail.com>
2025-03-27 22:30:51 +0000

    alter approach to one-page app with filtering

--- assets/js/database.js
+++ assets/js/database.js
@@ -1,25 +1,13 @@
 const ocarinas = [
     {
-        name: "",
-        country: "",
-        time: "",
-        maker: "",
-        material: "",
-        description: "",
-        images: ["", ""],
-        video: "",
-        audio: ""
+        country: "Mexico", date: "Pre-1500 AD", maker: "Zacatenco Culture", material: "Clay",
+        description: "This ancient, clay ocarina was made sometime between 1000 and 400 BC by the Zacatenco culture, in what is now central Mexico.",
+        images: ["", ""]
     },
     {
-        name: "",
-        country: "",
-        time: "",
-        maker: "",
-        material: "",
-        description: "",
-        images: ["", ""],
-        video: "",
-        audio: ""
+        country: "Mexico", date: "Pre-1500 AD", maker: "Colima Culture", material: "Clay",
+        description: "This clay ocarina was made by the Colima culture sometime between 100 BC and 250 AD.",
+        images: ["", ""]
     }
 ];
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,74 @@
-import ocarinas from "./database";
+import ocarinas from "./database.js";
+
+
+window.addEventListener("load", () => {
+
+    const ocarinaRiver = document.getElementById("ocarina-river");
+
+    const countrySelector = document.getElementById("countries");
+    const dateSelector = document.getElementById("dates");
+    const makerSelector = document.getElementById("makers");
+    const materialSelector = document.getElementById("materials");
+
+
+    function renderOcarinas() {
+
+        const selectedCountry = countrySelector.value;
+        const selectedDate = dateSelector.value;
+        const selectedMaker = makerSelector.value;
+        const selectedMaterial = materialSelector.value;
+
+        ocarinaRiver.innerHTML = "";
+
+        ocarinas.forEach((ocarina, index) => {
+            const matchesCountry = selectedCountry === "all" || ocarina.country === selectedCountry;
+            const matchesDate = selectedDate === "all" || ocarina.date === selectedDate;
+            const matchesMaker = selectedMaker === "all" || ocarina.maker === selectedMaker;
+            const matchesMaterial = selectedMaterial === "all" || ocarina.material === selectedMaterial;
+
+            if (matchesCountry && matchesDate && matchesMaker && matchesMaterial) {
+                const ocarinaCard = document.createElement("div");
+                ocarinaCard.classList.add("ocarina-card");
+                ocarinaCard.innerHTML = `
+                    <div class="img-space"></div>
+                    <br>
+                    <p>
+                        <i class="fa-solid fa-earth-americas" title="Country"></i>
+                        ${ocarina.country}
+                    </p>
+                    <p>
+                        <i class="fa-solid fa-timeline" title="Date"></i>
+                        ${ocarina.date}
+                    </p>
+                    <p>
+                        <i class="fa-solid fa-hand" title="Maker or Origin"></i>
+                        ${ocarina.maker}
+                    </p>
+                    <p>
+                        <i class="fa-solid fa-cube" title="Material"></i>
+                        ${ocarina.material}
+                    </p>
+                    <br>
+                    <p class="read-more" data-index="${index}"> Read more >> </p>
+                `;
+                ocarinaRiver.appendChild(ocarinaCard);
+            }
+        });
+
+        const moreButtons = document.getElementsByClassName("read-more");
+        for (let moreButton of moreButtons) {
+            moreButton.addEventListener("click", (event) => {
+                let ocarina = ocarinas[event.target.dataset.index];
+                console.log(ocarina);
+            });
+        }
+    }
+
+    renderOcarinas();
+
+    countrySelector.addEventListener("change", renderOcarinas);
+    dateSelector.addEventListener("change", renderOcarinas);
+    makerSelector.addEventListener("change", renderOcarinas);
+    materialSelector.addEventListener("change", renderOcarinas);
+
+});
